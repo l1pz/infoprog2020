b6405b1e2ba7c698dc06c5209289d0cd7a221dbd
Matthew Miller <l1pz@pop-os.localdomain>
2020-01-24 23:37:06 +0100

    removed objs from git

--- bin-obj/Debug-linux-x86_64/Latnivalo.d
+++
@@ -1,6 +0,0 @@
-../bin-obj/Debug-linux-x86_64/Latnivalo.o: ../src/Latnivalo.cpp \
- ../src/pch.h ../src/Latnivalo.h
-
-../src/pch.h:
-
-../src/Latnivalo.h:
--- bin-obj/Debug-linux-x86_64/pch.h.d
+++
@@ -1,1 +0,0 @@
-../bin-obj/Debug-linux-x86_64/pch.h.gch: ../src/pch.h
